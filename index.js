0ae68568e176324e53a7b7cd8a0cd9f72c0de38c
Linus Oleander <linus@oleander.nu>
2015-11-18 01:02:14 +0100

    Add tree parser

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var threads = require("./threads");
-
-threads("https://www.flashback.org/t2648395").then(function(posts){
-  console.info("posts ===> ", posts);
+var traverseTree = require("./traverseTree");
+threads("https://www.flashback.org/t2647929", 1, {}, null).then(function(data){
+  console.info("digraph graphname { ")
+  console.info("overlap=scale;");
+  console.info('size="7.75,10.25";');
+  console.info("nodesep=2.0;");
+  traverseTree(data.tree, data.root);
+  console.info("}")
 }).catch(function(err){
   console.info("error", err);
-})
+});
+
--- threads.js
+++ threads.js
@@ -1,36 +1,47 @@
 var get = require("./get");
 var cheerio = require("cheerio");
 
-var getPosts = function(url, page, working) {
-  page = page || 1;
-  working = working || {};
-  console.info("page", page)
+var getPosts = function(url, page, working, root, maxPages) {
   return get(url + "p" + page).then(function(body) {
     var $ = cheerio.load(body);
     var result = $(".delim").text().match(/Sidan (\d+) av (\d+)/);
-    var posts = [];
+    if(!root && page == 1) {
+      root = "p" + $("table.post").first().find(".post_message").attr("id").split("_")[2];
+    }
+
+    if(!root) {
+      throw "no root node found on page " + page;
+    }
+
     $("table.post").each(function(index, post) {
       var id = "p" + $(this).find(".post_message").attr("id").split("_")[2];
+      // Ignore root node
+      if(id == root) { return; }
       var cit = $(this).find(".alt2.post-quote > div > a").attr("href");
       if(cit) {
-        var ccid = cit.split("#")[1]
+        var ccid = cit.split("#")[1];
         if(!working[ccid]){
           working[ccid] = [];
         }
         working[ccid].push(id);
       } else {
-        if(!working.root) {
-          working.root = [];
+        if(!working[root]) {
+          working[root] = [];
         }
-        working.root.push(id);
+        working[root].push(id);
       }
     });
 
-    if(result[1] === result[2]){
-      return working;
+    // Abort if last page
+    if(result[1] === result[2] || maxPages === 0){
+      return { tree: working, root: root };
     }
 
-    return getPosts(url, page + 1, working);
+    if(maxPages) {
+      maxPages--;
+    }
+
+    return getPosts(url, page + 1, working, root, maxPages);
   });
 };
 
